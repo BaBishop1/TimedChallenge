df9667a573f8d6b76b044b836fcb201a12b71a43
NAHowell <nahowell2321@hotmail.com>
2023-04-11 10:35:25 -0400

    commit 2 day 2

--- TimedChallenge.Services/Post/IPostService.cs
+++ TimedChallenge.Services/Post/IPostService.cs
@@ -1,4 +1,4 @@
 public interface IPostService
 {
-    
+    Task<bool> RegisterPostAsync()
 }
--- TimedChallenge.Services/Reply/ReplyService.cs
+++ TimedChallenge.Services/Reply/ReplyService.cs
@@ -1,4 +1,31 @@
-public class ReplySerice : IReplyService
+using TimedChallenge.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+public class ReplyService : IReplyService
 {
-    
+    private readonly ApplicationDbContext _context;
+
+    public ReplyService(ApplicationDbContext context) => _context = context;
+
+    public async Task<bool> RegisterReplyAsync(ReplyRegister model)
+    {
+        var entity = new Reply 
+        {
+            ReplyText = model.ReplyText,
+            AuthorID = model.AuthorID,
+            DateCreated = model.DateCreated
+        };
+
+        _context.Reply.Add(entity);
+        var numberOfChanges = await _context.SaveChangesAsync();
+
+        return numberOfChanges == 1; 
+
+    }
+
+    private async Task<Reply> GetReplyAuthorIDAsync (Guid authorID)
+    {
+        return await _context.Reply.FirstOrDefaultAsync(reply => reply.AuthorID == authorID);
+    }
+
 }
